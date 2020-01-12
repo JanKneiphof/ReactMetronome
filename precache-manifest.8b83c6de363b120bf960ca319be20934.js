b379974935218513a06a63e3836b8ac1d8823dac
JanKneiphof <JanKneiphof@users.noreply.github.com>
2020-01-12 18:05:01 +0100

    Updates

---
+++ precache-manifest.8b83c6de363b120bf960ca319be20934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7301dbd996ef71035c859a97050f02e",
+    "url": "/ReactMetronome/index.html"
+  },
+  {
+    "revision": "0a8157d7d6400141f75f",
+    "url": "/ReactMetronome/static/css/main.1779617f.chunk.css"
+  },
+  {
+    "revision": "75c43cbcb9bcaeef0f68",
+    "url": "/ReactMetronome/static/js/2.cd0c2495.chunk.js"
+  },
+  {
+    "revision": "147b67535779bbdb75aea80f3df0da0f",
+    "url": "/ReactMetronome/static/js/2.cd0c2495.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a8157d7d6400141f75f",
+    "url": "/ReactMetronome/static/js/main.8d931ca7.chunk.js"
+  },
+  {
+    "revision": "16c24240db2e98378051",
+    "url": "/ReactMetronome/static/js/runtime-main.6c5a3232.js"
+  }
+]);
