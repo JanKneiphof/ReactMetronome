5668d042f6758f22daad69dd3c8ee9909a508fcc
JanKneiphof <JanKneiphof@users.noreply.github.com>
2020-01-12 17:45:14 +0100

    Add BpmInput Component with Tests

---
+++ src/BpmInput.js
@@ -0,0 +1,112 @@
+import { Button, Grid, InputAdornment, TextField } from "@material-ui/core";
+import React, { Component } from 'react';
+
+const noCapslockButtonText = ({
+    textTransform: 'none'
+});
+
+class BpmInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: this.props.defaultBpm,
+        };
+    }
+
+    addBpm(value) {
+        const number = parseInt(this.state.value, 10) + parseInt(value, 10)
+        if (number < 0) {
+            return
+        }
+        else {
+            this.setState(
+                { value: number },
+                this.updateMetronomeBpm(number)
+            )
+        }
+    }
+
+    updateMetronomeBpm(number) {
+        this.props.updateBpm(number)
+    }
+
+    multiplyBpmBy(value) {
+        const number = Math.floor(parseInt(this.state.value, 10) * parseFloat(value, 10))
+        if (number < 0) {
+            return
+        } else {
+            this.setState(
+                { value: number },
+                this.updateMetronomeBpm(number)
+            )
+        }
+    }
+
+    isPositiveInteger(number) {
+        if (isNaN(number) || number < 0) {
+            return false
+        }
+        else {
+            return true
+        }
+    }
+
+    handleInput(inputEvent) {
+        const number = inputEvent.target.value
+        if (this.isPositiveInteger(number)) {
+            this.setState(
+                { value: number.trim() },
+                this.updateMetronomeBpm(number)
+            )
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Grid container spacing={1} alignItems="center" justify="center" direction="row">
+                    <Grid item>
+                        <TextField
+                            InputProps={{
+                                startAdornment: <InputAdornment position="start">♩ = </InputAdornment>,
+                                //endAdornment: <InputAdornment position='end'>Beat Units per Minute</InputAdornment>
+                            }}
+                            id="username"
+                            onChange={this.handleInput.bind(this)}
+                            value={this.state.value}
+                            placeholder="0"
+                            margin="normal"
+                            label="Enter BPM"
+                            variant="outlined"
+                            autoFocus={true} />
+                    </Grid>
+                    <Grid item>
+                        <Grid alignContent="center" justify="center" container spacing={1} direction="row">
+                            <Grid item>
+                                <Button aria-label="+10" onClick={this.addBpm.bind(this, '+10')} variant="outlined">+10</Button>
+                            </Grid>
+                            <Grid item>
+                                <Button aria-label="x1.5" style={noCapslockButtonText} onClick={this.multiplyBpmBy.bind(this, '1.5')} variant="outlined">x1.5</Button>
+                            </Grid>
+                            <Grid item>
+                                <Button aria-label="x2" style={noCapslockButtonText} onClick={this.multiplyBpmBy.bind(this, '2')} variant="outlined" >x2.0</Button>
+                            </Grid>
+                        </Grid>
+                        <Grid justify="center" container alignContent="center" spacing={1} direction="row">
+                            <Grid item>
+                                <Button aria-label="-10" onClick={this.addBpm.bind(this, '-10')} variant="outlined">-10</Button>
+                            </Grid>
+                            <Grid item>
+                                <Button aria-label="÷1.5" onClick={this.multiplyBpmBy.bind(this, '0.75')} variant="outlined">÷1.5</Button>
+                            </Grid>
+                            <Grid item>
+                                <Button aria-label="÷2" onClick={this.multiplyBpmBy.bind(this, '0.5')} variant="outlined">÷2</Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </div>
+        )
+    }
+}
+export default BpmInput
---
+++ src/tests/BpmInput.test.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BpmInput from '../BpmInput';
+import { act } from 'react-dom/test-utils'
+import myUserEvent from "./myUserTesting"
+import { getByLabelText } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+let container = document.createElement("div");
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+describe("BpmInput", () => {
+  test("displays default Value", async () => {
+    let inputElement
+    await act(async () => {
+      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="1234"></BpmInput>, container)
+      inputElement = getByLabelText(container, "Enter BPM")
+    });
+    expect(inputElement.value).toBe("1234")
+  })
+  test("can read typed Input", async () => {
+    let inputElement
+    await act(async () => {
+      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="120"></BpmInput>, container)
+      inputElement = getByLabelText(container, "Enter BPM")
+      await myUserEvent.type(inputElement, "333", { allAtOnce: false, delay: 1 });
+    });
+    expect(inputElement.value).toBe("120333")
+  })
+  test("can read pasted Input", async () => {
+    let inputElement
+    await act(async () => {
+      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="120"></BpmInput>, container)
+      inputElement = getByLabelText(container, "Enter BPM")
+      await myUserEvent.type(inputElement, "333", { allAtOnce: true });
+    });
+    expect(inputElement.value).toBe("120333")
+  })
+
+  test("doesn't accept Letters", async () => {
+    let inputElement
+    await act(async () => {
+      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="" ></BpmInput>, container);
+      inputElement = getByLabelText(container, "Enter BPM")
+      await myUserEvent.type(inputElement, "A1B1", { allAtOnce: false, delay: 1 })
+    })
+    expect(inputElement.value).toBe("11")
+  })
+
+  test("doesn't accept Whitespace", async () => {
+    let inputElement
+    await act(async () => {
+      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="120" ></BpmInput>, container);
+      inputElement = getByLabelText(container, "Enter BPM")
+      await myUserEvent.type(inputElement, " ", { allAtOnce: true })
+    })
+    expect(inputElement.value).toBe("120")
+  })
+
+  test("Adds 10BPM on +10 Button", () => {
+    act(() => {
+      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="110"></BpmInput>, container);
+      myUserEvent.click(getByLabelText(container, "+10"))
+    })
+    expect(getByLabelText(container, "Enter BPM").value).toBe("120")
+  })
+  test("Subtracts 10BPM on -10 Button", () => {
+    act( () => {
+      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="130"></BpmInput>, container);
+      myUserEvent.click(getByLabelText(container, "-10"))
+    })
+    expect(getByLabelText(container, "Enter BPM").value).toBe("120")
+  })
+  test("Multiplies BPM by 2 on x2 Button", () => {
+    act( () => {
+      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="60"></BpmInput>, container);
+      myUserEvent.click(getByLabelText(container, "x2"))
+    })
+    expect(getByLabelText(container, "Enter BPM").value).toBe("120")
+  })
+  test("Multiplies BPM by 1.5 on x1.5 Button", () => {
+    act( () => {
+      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="80"></BpmInput>, container);
+      myUserEvent.click(getByLabelText(container, "x1.5"))
+    })
+    expect(getByLabelText(container, "Enter BPM").value).toBe("120")
+  })
+  test("Divides BPM by 1.5 on ÷1.5 Button", () => {
+    act( () => {
+      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="160"></BpmInput>, container);
+      myUserEvent.click(getByLabelText(container, "÷1.5"))
+    })
+    expect(getByLabelText(container, "Enter BPM").value).toBe("120")
+  })
+  test("Divides BPM by 2 on ÷2 Button", () => {
+    act( () => {
+      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="240"></BpmInput>, container);
+      myUserEvent.click(getByLabelText(container, "÷2"))
+    })
+    expect(getByLabelText(container, "Enter BPM").value).toBe("120")
+  })
+})
