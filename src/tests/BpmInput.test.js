0b9b4016d4bb12144ba15a311458d85a06c94c09
JanKneiphof <JanKneiphof@users.noreply.github.com>
2020-02-01 22:46:21 +0100

    add tempoStyle prop

--- src/tests/BpmInput.test.js
+++ src/tests/BpmInput.test.js
@@ -19,16 +19,16 @@ describe("BpmInput", () => {
   test("displays default Value", async () => {
     let inputElement
     await act(async () => {
-      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="1234"></BpmInput>, container)
-      inputElement = getByLabelText(container, "Enter BPM")
+      ReactDOM.render(<BpmInput tempoStyle="Quarter" updateBpm={() => {}} defaultBpm="1234"></BpmInput>, container)
+      inputElement = getByLabelText(container, "Enter Tempo")
     });
     expect(inputElement.value).toBe("1234")
   })
   test("can read typed Input", async () => {
     let inputElement
     await act(async () => {
-      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="120"></BpmInput>, container)
-      inputElement = getByLabelText(container, "Enter BPM")
+      ReactDOM.render(<BpmInput tempoStyle="Quarter" updateBpm={() => {}} defaultBpm="120"></BpmInput>, container)
+      inputElement = getByLabelText(container, "Enter Tempo")
       await myUserEvent.type(inputElement, "333", { allAtOnce: false, delay: 1 });
     });
     expect(inputElement.value).toBe("120333")
@@ -36,8 +36,8 @@ describe("BpmInput", () => {
   test("can read pasted Input", async () => {
     let inputElement
     await act(async () => {
-      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="120"></BpmInput>, container)
-      inputElement = getByLabelText(container, "Enter BPM")
+      ReactDOM.render(<BpmInput tempoStyle="Quarter" updateBpm={() => {}} defaultBpm="120"></BpmInput>, container)
+      inputElement = getByLabelText(container, "Enter Tempo")
       await myUserEvent.type(inputElement, "333", { allAtOnce: true });
     });
     expect(inputElement.value).toBe("120333")
@@ -46,8 +46,8 @@ describe("BpmInput", () => {
   test("doesn't accept Letters", async () => {
     let inputElement
     await act(async () => {
-      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="" ></BpmInput>, container);
-      inputElement = getByLabelText(container, "Enter BPM")
+      ReactDOM.render(<BpmInput tempoStyle="Quarter" updateBpm={() => {}} defaultBpm="" ></BpmInput>, container);
+      inputElement = getByLabelText(container, "Enter Tempo")
       await myUserEvent.type(inputElement, "A1B1", { allAtOnce: false, delay: 1 })
     })
     expect(inputElement.value).toBe("11")
@@ -56,8 +56,8 @@ describe("BpmInput", () => {
   test("doesn't accept Whitespace", async () => {
     let inputElement
     await act(async () => {
-      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="120" ></BpmInput>, container);
-      inputElement = getByLabelText(container, "Enter BPM")
+      ReactDOM.render(<BpmInput tempoStyle="Quarter" updateBpm={() => {}} defaultBpm="120" ></BpmInput>, container);
+      inputElement = getByLabelText(container, "Enter Tempo")
       await myUserEvent.type(inputElement, " ", { allAtOnce: true })
     })
     expect(inputElement.value).toBe("120")
@@ -65,44 +65,44 @@ describe("BpmInput", () => {
 
   test("Adds 10BPM on +10 Button", () => {
     act(() => {
-      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="110"></BpmInput>, container);
+      ReactDOM.render(<BpmInput tempoStyle="Quarter" updateBpm={() => {}} defaultBpm="110"></BpmInput>, container);
       userEvent.click(getByLabelText(container, "+10"))
     })
-    expect(getByLabelText(container, "Enter BPM").value).toBe("120")
+    expect(getByLabelText(container, "Enter Tempo").value).toBe("120")
   })
   test("Subtracts 10BPM on -10 Button", () => {
     act( () => {
-      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="130"></BpmInput>, container);
+      ReactDOM.render(<BpmInput tempoStyle="Quarter" updateBpm={() => {}} defaultBpm="130"></BpmInput>, container);
       userEvent.click(getByLabelText(container, "-10"))
     })
-    expect(getByLabelText(container, "Enter BPM").value).toBe("120")
+    expect(getByLabelText(container, "Enter Tempo").value).toBe("120")
   })
   test("Multiplies BPM by 2 on x2 Button", () => {
     act( () => {
-      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="60"></BpmInput>, container);
+      ReactDOM.render(<BpmInput tempoStyle="Quarter" updateBpm={() => {}} defaultBpm="60"></BpmInput>, container);
       userEvent.click(getByLabelText(container, "x2"))
     })
-    expect(getByLabelText(container, "Enter BPM").value).toBe("120")
+    expect(getByLabelText(container, "Enter Tempo").value).toBe("120")
   })
   test("Multiplies BPM by 1.5 on x1.5 Button", () => {
     act( () => {
-      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="80"></BpmInput>, container);
+      ReactDOM.render(<BpmInput tempoStyle="Quarter" updateBpm={() => {}} defaultBpm="80"></BpmInput>, container);
       userEvent.click(getByLabelText(container, "x1.5"))
     })
-    expect(getByLabelText(container, "Enter BPM").value).toBe("120")
+    expect(getByLabelText(container, "Enter Tempo").value).toBe("120")
   })
   test("Divides BPM by 1.5 on ÷1.5 Button", () => {
     act( () => {
-      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="160"></BpmInput>, container);
+      ReactDOM.render(<BpmInput tempoStyle="Quarter" updateBpm={() => {}} defaultBpm="160"></BpmInput>, container);
       userEvent.click(getByLabelText(container, "÷1.5"))
     })
-    expect(getByLabelText(container, "Enter BPM").value).toBe("120")
+    expect(getByLabelText(container, "Enter Tempo").value).toBe("120")
   })
   test("Divides BPM by 2 on ÷2 Button", () => {
     act( () => {
-      ReactDOM.render(<BpmInput updateBpm={() => {}} defaultBpm="240"></BpmInput>, container);
+      ReactDOM.render(<BpmInput tempoStyle="Quarter" updateBpm={() => {}} defaultBpm="240"></BpmInput>, container);
       userEvent.click(getByLabelText(container, "÷2"))
     })
-    expect(getByLabelText(container, "Enter BPM").value).toBe("120")
+    expect(getByLabelText(container, "Enter Tempo").value).toBe("120")
   })
 })
