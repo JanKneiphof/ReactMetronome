12c9a2502ae8638a5612a977afd559d80e219a0d
JanKneiphof <JanKneiphof@users.noreply.github.com>
2020-02-01 22:07:14 +0100

    update Default Parameter

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <Metronome defaultSubdivisionsPerBeat="1" defaultBeatUnit="4" defaultBeatsPerMeasure="4" defaultBpm="120"></Metronome>
+      <Metronome
+        defaultSubdivisionsPerBeat="1"
+        defaultBeatUnit="4"
+        defaultBeatsPerMeasure="4"
+        defaultBpm="120"
+        defaultBeatAccentuation={new Map([[0,3],[1,1],[2,1],[3,1]])}></Metronome>
     </div>
   );
 }
