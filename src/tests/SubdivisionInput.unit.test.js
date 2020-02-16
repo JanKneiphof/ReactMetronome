9407e17124d7e105381c683055f8acb936e7c6d1
JanKneiphof <JanKneiphof@users.noreply.github.com>
2020-02-16 16:48:49 +0100

    add test

--- src/tests/SubdivisionInput.unit.test.js
+++ src/tests/SubdivisionInput.unit.test.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import SubdivisionInput from '../Components/SubdivisionInput'
-import { getAllByLabelText } from '@testing-library/react';
+import { getAllByLabelText, getByLabelText } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 let container = document.createElement("div");
 beforeEach(() => {
@@ -14,10 +15,18 @@ afterEach(() => {
     container = null;
 });
 
+function createSubdivisionInput(changeSubdivisionMock, beatAccentuation, numberOfSubdivisions) {
+    return (<SubdivisionInput
+        changeSubdivision={changeSubdivisionMock}
+        beatAccentuation={beatAccentuation}
+        numberOfSubdivisions={numberOfSubdivisions}>
+    </SubdivisionInput>)
+}
+
 describe("SubdivisionInput", () => {
     test("matches Snapshot", () => {
         act(() => {
-            ReactDOM.render(<SubdivisionInput changeSubdivision={() => {}} beatAccentuation={new Map([[0,3],[1,1],[2,1],[3,1]])} numberOfSubdivisions="4"></SubdivisionInput>, container)
+            ReactDOM.render(createSubdivisionInput(() => {}, new Map([[0, 3], [1, 1], [2, 1], [3, 1]]), "4"), container)
         });
         expect(container.firstChild).toMatchSnapshot()
     })
@@ -25,15 +34,24 @@ describe("SubdivisionInput", () => {
     test("renders correct amount of Buttons", () => {
         let correctAmount = 4
         act(() => {
-            ReactDOM.render(<SubdivisionInput changeSubdivision={() => {}} beatAccentuation={new Map()} numberOfSubdivisions={correctAmount}></SubdivisionInput>, container)
+            ReactDOM.render(<SubdivisionInput changeSubdivision={() => { }} beatAccentuation={new Map()} numberOfSubdivisions={correctAmount}></SubdivisionInput>, container)
         })
         expect(getAllByLabelText(container, /Subdivision \d/).length).toBe(correctAmount)
     })
     test("renders correct amount of Buttons, even with big numbers", () => {
         let correctAmount = 123
         act(() => {
-            ReactDOM.render(<SubdivisionInput changeSubdivision={() => {}} beatAccentuation={new Map()} numberOfSubdivisions={correctAmount}></SubdivisionInput>, container)
+            ReactDOM.render(<SubdivisionInput changeSubdivision={() => { }} beatAccentuation={new Map()} numberOfSubdivisions={correctAmount}></SubdivisionInput>, container)
         })
         expect(getAllByLabelText(container, /Subdivision \d/).length).toBe(correctAmount)
+    })
+    test("calls update function on Buttonclick", () => {
+        var updateMock = jest.fn()
+        act(() => {
+            ReactDOM.render(createSubdivisionInput(updateMock, new Map([[0, 3], [1, 1], [2, 1], [3, 1]]), "4"), container)
+            userEvent.click(getByLabelText(container, "Subdivision 2"))
+        })
+        expect(updateMock.mock.calls.length).toBe(1)
+        expect(updateMock.mock.calls[0][0]).toBe(2)
     })
 })
