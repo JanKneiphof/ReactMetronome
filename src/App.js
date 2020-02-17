c3d934011240e2e6a783da7cd1d1ff2d49eab30c
JanKneiphof <JanKneiphof@users.noreply.github.com>
2020-02-17 11:22:58 +0100

    add Link to Github Repository

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Grid, Link, Typography } from '@material-ui/core';
 import React from 'react';
 import Metronome from './Components/Metronome';
 import Navbar from './Components/Navbar';
@@ -6,14 +7,24 @@ function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <Metronome
-        defaultSubdivisionsPerBeat="1"
-        defaultBeatUnit="4"
-        defaultBeatsPerMeasure="4"
-        defaultBpm="120"
-        defaultBeatAccentuation={new Map([[0,3],[1,1],[2,1],[3,1]])}
-        tempoStyle="Quarter">
-        </Metronome>
+      <Grid container direction="column" alignItems="center">
+        <Grid item>
+          <Metronome
+            defaultSubdivisionsPerBeat="1"
+            defaultBeatUnit="4"
+            defaultBeatsPerMeasure="4"
+            defaultBpm="120"
+            defaultBeatAccentuation={new Map([[0, 3], [1, 1], [2, 1], [3, 1]])}
+            tempoStyle="Quarter">
+          </Metronome>
+        </Grid>
+        <Grid item>
+          <Typography>
+            Check out the Code on&nbsp;
+              <Link href="https://github.com/JanKneiphof/ReactMetronome" target="_blank" rel="noreferrer">Github</Link>
+          </Typography>
+        </Grid>
+      </Grid>
     </div>
   );
 }
