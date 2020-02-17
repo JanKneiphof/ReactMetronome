f24ca865956f1340f8ecae7afb4ed4cecd1b61bf
JanKneiphof <JanKneiphof@users.noreply.github.com>
2020-02-17 11:58:20 +0100

    add MIDISounds Component in render()

--- src/Components/MidiPlayback.js
+++ src/Components/MidiPlayback.js
@@ -1,9 +1,14 @@
-import { Component } from "react";
+import MIDISounds from 'midi-sounds-react';
+import React, { Component } from 'react';
 
 class MidiPlayback extends Component {
     render() {
         return (
-            <div></div>
+            <MIDISounds
+                ref={(ref) => (this.midiSounds = ref)}
+                appElementName="root"
+                drums={[200, 205, 210]}>
+            </MIDISounds>
         )
     }
 }
